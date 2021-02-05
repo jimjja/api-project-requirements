51ff14b4b1dca3bcd669d8abeaf4b4786981835b
munamohamed <munamohamed@MUNAMOHAMEDA553>
2021-02-05 14:08:31 +0000

    Create models

---
+++ Models/Beverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShalekKavy.Api
+{
+    public class Beverage
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public BeverageType BeverageType { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Allergens { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+
+}
