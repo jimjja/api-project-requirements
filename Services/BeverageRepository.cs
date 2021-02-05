2c0ca98be1728f64c5b879175bd8d44b2c7e1c0f
munamohamed <munamohamed@MUNAMOHAMEDA553>
2021-02-05 14:09:43 +0000

    Set up data service using repository pattern, add controller

---
+++ Services/BeverageRepository.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShalekKavy.Api.Services
+{
+    public class BeverageRepository : IBeverageRepository
+    {
+        public List<Beverage> GetAll()
+        {
+            // read file and deserialize json
+            var contentRootPath = Directory.GetCurrentDirectory();
+
+            var data = File.ReadAllText(contentRootPath + "/Data/Beverages.json");
+
+            var deserializeContent = JsonConvert.DeserializeObject<List<Beverage>>(data);
+
+            return deserializeContent;
+        }
+
+        public IEnumerable<Beverage> GetById(string id)
+        {
+            
+            var contentRootPath = Directory.GetCurrentDirectory();
+
+            var data = File.ReadAllText(contentRootPath + "/Data/Beverages.json");
+
+            var deserializeContent = JsonConvert.DeserializeObject<List<Beverage>>(data);
+
+            var beverage = deserializeContent.Where(x => x.Id == id);
+
+            return beverage;
+        }
+
+        public IEnumerable<Beverage> GetByBeverage(string name)
+        {
+            // read file and deserialize json
+            var contentRootPath = Directory.GetCurrentDirectory();
+
+            var data = File.ReadAllText(contentRootPath + "/Data/Beverages.json");
+
+            var deserializeContent = JsonConvert.DeserializeObject<List<Beverage>>(data);
+
+            var beverage = deserializeContent.Where(x => x.Name == name);
+
+            return beverage;
+        }
+
+        public IEnumerable<Beverage> GetByBeverageType(BeverageType type)
+        {
+            // read file and deserialize json
+            var contentRootPath = Directory.GetCurrentDirectory();
+
+            var data = File.ReadAllText(contentRootPath + "/Data/Beverages.json");
+
+            var deserializeContent = JsonConvert.DeserializeObject<List<Beverage>>(data);
+
+            var beverages = deserializeContent.Where(x => x.BeverageType == type);
+
+            return beverages;
+        }
+    }
+}
---
+++ Services/IBeverageRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShalekKavy.Api.Services
+{
+    public interface IBeverageRepository
+    {
+        public List<Beverage> GetAll();
+        public IEnumerable<Beverage> GetById(string id);
+        public IEnumerable<Beverage> GetByBeverage(string name);
+        public IEnumerable<Beverage> GetByBeverageType(BeverageType type);
+    }
+}
