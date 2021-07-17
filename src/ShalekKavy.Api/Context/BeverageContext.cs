aac779ec61979e387c150c33f776d95e401b34aa
Muna Mohamed <mmohamed@and.digital>
2021-07-17 22:05:52 +0100

    Define entity config to construct model for dbContext

--- src/ShalekKavy.Api/Context/BeverageContext.cs
+++ src/ShalekKavy.Api/Context/BeverageContext.cs
@@ -9,5 +9,23 @@ namespace ShalekKavy.Api.Context
     {
         public BeverageContext(DbContextOptions<BeverageContext> options) : base(options) { }
         public DbSet<Beverage> Beverages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var beverageModel = modelBuilder.Entity<Beverage>();
+
+            // Make ID the primary key
+            beverageModel.HasKey(x => x.Id);
+
+            beverageModel.Property(x => x.Name).IsRequired();
+            beverageModel.Property(x => x.Description).IsRequired();
+            beverageModel.Property(x => x.Ingredients).IsRequired();
+            beverageModel.Property(x => x.Availability).IsRequired();
+            beverageModel.Property(x => x.Size).IsRequired();
+            beverageModel.Property(x => x.Price).IsRequired();
+            beverageModel.Property(x => x.BeverageType).IsRequired();
+            beverageModel.Property(x => x.DateModified).IsRequired();
+            beverageModel.Property(x => x.DateCreated).IsRequired();
+        }
     }
 }
