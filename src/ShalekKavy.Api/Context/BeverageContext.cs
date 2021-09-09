2668e973881c67e1a6c7612dfa1487590629fab4
Muna Mohamed <mmohamed@and.digital>
2021-09-09 22:41:59 +0100

    Update modelBuilder in BeverageContext

--- src/ShalekKavy.Api/Context/BeverageContext.cs
+++ src/ShalekKavy.Api/Context/BeverageContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShalekKavy.Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,36 @@ namespace ShalekKavy.Api.Context
     {
         public BeverageContext(DbContextOptions<BeverageContext> options) : base(options) { }
         public DbSet<Beverage> Beverages { get; set; }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            var beverageModel = modelBuilder.Entity<Beverage>();
+            // set primary keys
+            modelBuilder.Entity<Beverage>()
+                .HasKey(x => x.Id);
 
-            // Make ID the primary key
-            beverageModel.HasKey(x => x.Id);
+            modelBuilder.Entity<AddOns>()
+                .HasKey(x => x.Id);
 
-            beverageModel.Property(x => x.Name).IsRequired();
-            beverageModel.Property(x => x.Description).IsRequired();
-            beverageModel.Property(x => x.Ingredients).IsRequired();
-            beverageModel.Property(x => x.Availability).IsRequired();
-            beverageModel.Property(x => x.Size).IsRequired();
-            beverageModel.Property(x => x.Price).IsRequired();
-            beverageModel.Property(x => x.BeverageType).IsRequired();
-            beverageModel.Property(x => x.DateModified).IsRequired();
-            beverageModel.Property(x => x.DateCreated).IsRequired();
+            // set up fields for beverages and addon
+            modelBuilder.Entity<Beverage>().Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Name).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Description).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Ingredients).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Allergens).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Availability).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Allergens).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Size).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.Price).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.DateCreated).IsRequired();
+            modelBuilder.Entity<Beverage>().Property(x => x.DateModified).IsRequired();
+
+            modelBuilder.Entity<AddOns>().Property(x => x.Name).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.Availability).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.Type).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.BeverageId).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.Price).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.DateCreated).IsRequired();
+            modelBuilder.Entity<AddOns>().Property(x => x.DateModified).IsRequired();
         }
+
     }
 }
